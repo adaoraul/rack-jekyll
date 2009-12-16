37c7fef4561102d743be3eeee658ebdceea18239
bry4n <bryan@ffiirree.com>
2009-12-16 04:32:16 -0600

    added test

---
+++ test/spec_rack-jekyll.rb
@@ -0,0 +1,56 @@
+require 'bacon'
+require 'rack/mock'
+require "lib/rack/jekyll"
+
+def get(klass, path)
+  request = Rack::MockRequest.new(klass)
+  request.get(path)
+end
+
+describe "Jekyll to Rack" do
+  
+  rackup = File.dirname(__FILE__) + "/config.ru"
+  jekyll = Rack::Jekyll::Test.new(rackup)
+  
+  it "knows the config file" do
+    jekyll.config.should.equal rackup
+  end
+  
+  it "should be 200 and not 404" do
+    res = get(jekyll,"/")
+    res.status.should.equal 200
+    res.status.should.not.equal 404
+  end
+
+  it "should be 11" do
+    res = get(jekyll,"/")
+    res.headers["Content-Length"].to_i.should.equal 11
+  end
+
+  it "should be text/html" do
+    res = get(jekyll,"/")
+    res.headers["Content-Type"].should.equal "text/html"
+  end
+  
+  it "should show Jekyll/Rack" do
+    res = get(jekyll,"/")
+    res.body.should.equal "Jekyll/Rack"
+  end
+
+  it "should be 404 and not 200" do
+    res = get(jekyll,"/show/me/404")
+    res.status.should.not.equal 200
+    res.status.should.equal 404
+  end
+  
+  it "should be text/css" do
+    res = get(jekyll,"/css/test.css")
+    res.headers["Content-Type"].should.equal "text/css"
+  end
+  
+  it "should be application/javascript" do
+    res = get(jekyll,"/js/test.js")
+    res.headers["Content-Type"].should.equal "application/javascript"
+  end
+
+end
