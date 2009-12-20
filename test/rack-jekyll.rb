7a04be175f96657d15fdccfd56c6bfcc3aeee05a
bry4n <bryan@ffiirree.com>
2009-12-20 16:00:52 -0600

    more tests

--- test/rack-jekyll.rb
+++ test/rack-jekyll.rb
@@ -8,9 +8,16 @@ class RackJekyllTest < Test::Unit::TestCase
     @request = Rack::MockRequest.new(@jekyll)
   end
   
+  def test_setup
+    assert_not_nil @jekyll
+    assert_not_nil @request
+  end
+
   def test_http_status_codes
     assert_equal(@request.get("/").status, 200)
+    assert_not_equal(@request.get("/").status,404)
     assert_equal(@request.get("/show/me/404").status,404)
+    assert_not_equal(@request.get("/show/me/404").status,200)
   end
 
   def test_content_types
@@ -19,6 +26,11 @@ class RackJekyllTest < Test::Unit::TestCase
     assert_equal(@request.get("/js/test.js").headers["Content-Type"],"application/javascript")
   end
 
+  def test_content_length
+    assert_equal(@request.get("/").headers["Content-Length"].to_i, 11)
+    assert_not_equal(@request.get("/").headers["Content-Length"].to_i, 12)
+  end
+  
   def test_contents 
     assert_equal(@request.get("/").body,"Jekyll/Rack")
     assert_equal(@request.get("/show/me/404").body,"Not found")
